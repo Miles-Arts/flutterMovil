c619611ca4705d908b7101a68148eba742e06bfc
Milton Figueredo Miles <gratno@gmail.com>
2024-05-23 14:02:27 -0500

    Example types of maps and extructure

--- 04_maps.dart
+++ 04_maps.dart
@@ -3,27 +3,32 @@ void main() {
   
   //creación de un MAP
   //Key Value
-  final pokemon = {
+  //final  Map<String, dynamic>pokemon = {
+  final  pokemon = {
     'name': 'Ditton',
     'hp': 100,
     'isAlive': true,
     'abilities': <String>['impostor'],
-    'sprite': {
+    'sprites': {
       1: 'ditto/front.png',
-      2: 'ditto/back.png',
+      2: 'ditto/back.png'
       
     }
     
   };
-  
+  /*
   final pokemons = {
     1: 'Blue',
     2: 'WHITE',
     3: 'PINK'
     
-  };
+  */
   
   
   print(pokemon);
-  print(pokemons);
+  print('Name: ${ pokemon['name'] }');
+  print('Name: ${ pokemon['sprites'] }');
+  print('Back; ${ pokemon['sprites'[1]]}');
+  print('Front; ${ pokemon['sprites'[1]]}');
+  
 }
