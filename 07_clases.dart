52d8d47e96bdf746c9d3b4a5ab844172c9bd2d80
Milton Figueredo Miles <gratno@gmail.com>
2024-05-24 11:21:59 -0500

    Examples

---
+++ 07_clases.dart
@@ -0,0 +1,36 @@
+void main() {
+   
+    final wolverine = Hero('Logan', 'Regeneración');
+  
+  print( wolverine );
+  print( wolverine.name );
+  print( wolverine.power );
+  
+}
+
+class Hero {
+  
+  String name;
+  String power;
+  
+  Hero( this.name, this.power );
+  
+  /*
+  Hero( String pName, String pPower )
+    : name = pName,
+      power = pPower;
+    
+    
+  {
+    
+   
+    //THIS es opcional
+    this.name = pName;
+    this.power = pPower;
+    
+
+    name = pName;
+    power = pPower;
+    
+  }*/
+}
